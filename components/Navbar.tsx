aa30dc409cdfaf3097cf908d8c1e505df4365813
Aryan <aryansonwatikar@gmail.com>
2025-07-21 17:30:35 +0530

    feat: replaced bg blurring with adjusting bg opacity on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,25 +3,24 @@
 import React from 'react';
 
 const Navbar = () => {
-  // const [scrollY, setScrollY] = React.useState(0);
+  const [scrollY, setScrollY] = React.useState(0);
 
-  // React.useEffect(() => {
-  //   const handleScroll = () => {
-  //     setScrollY(window.scrollY);
-  //   };
-  //   window.addEventListener('scroll', handleScroll);
-  //   return () => window.removeEventListener('scroll', handleScroll);
-  // }, []);
-  // const blurAmount = Math.min(scrollY / 10, 10);
+  React.useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  const opacity = Math.min(scrollY / 400, 0.6);
 
   return (
     <div
-      // style={{
-      //   backdropFilter: `blur(${blurAmount}px)`,
-      //   WebkitBackdropFilter: `blur(${blurAmount}px)`,
-      //   transition: 'backdrop-filter 0.3s',
-      // }}
-      className={`flex z-50 items-center justify-evenly w-full fixed top-0 left-0 py-3 bg-transparent mask-light-fade-bottom`}
+      style={{
+        backgroundColor: `rgba(17, 24, 39, ${opacity})`, // Tailwind gray-900
+        transition: 'background-color 0.2s',
+      }}
+      className={`flex z-50 items-center justify-evenly w-full fixed top-0 left-0 py-3 mask-light-fade-bottom`}
     >
       <h1 className='text-4xl font-bold'>the movie diary.</h1>
     </div>
