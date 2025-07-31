3e93d5fa29d94f472e52fd917e5de252a98b5a2c
Aryan <aryansonwatikar@gmail.com>
2025-07-31 19:11:36 +0530

    feat: added Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+    <footer className="w-full py-4 text-center text-sm text-gray-400 bg-transparent border-t border-t-gray-800 mt-8">
+        <span> © {new Date().getFullYear()} - the movie diary</span>
+    </footer>
+);
+
+export default Footer;
